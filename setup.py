9503bd9b742107147218679b716573f40d53fc51
jorik2603 <150458572+jorik2603@users.noreply.github.com>
2025-08-14 16:14:00 +0200

    Update setup.py
    added contact information

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="spectro-suite",
-    version="1.0.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    name="SpectroSuite",
+    version="0.1.0",
+    author="Jorik Schaap",
+    author_email="j.h.schaap@utwente.nl",
     description="A Python GUI for plotting and comparing 2D spectroscopy datasets.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/spectro-suite", # Replace with your URL
+    url="https://github.com/jorik2603/SpectroSuite", # Replace with your URL
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
